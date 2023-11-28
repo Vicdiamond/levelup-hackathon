ad551634c1fbce5d3df4081af0afd2f0b325c273
Dev <97352849+Vicdiamond@users.noreply.github.com>
2023-11-29 00:13:43 +0100

    Finished most of the functionalities

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const contents = document.querySelectorAll('.main-form-body-1-content-p')
 const main = document.querySelector('.main-form-body')
 const checkedImages = document.querySelector('.checked-img')
 const checkContainers = document.querySelectorAll('.check-container')
+const checkBoxes = document.querySelectorAll('.checkbox')
+const progress = document.querySelector('.prog-bar')
+const progEl = document.querySelector('.prog-el')
+const myStoreContent = document.querySelector('.my-store')
+const alertContent = document.querySelector('.alert')
 
 // close trial section
 document
@@ -28,94 +33,124 @@ document
     })
   })
 
-// let clicked
+// Toggle for the store section
+let active = true
+document
+  .querySelector('.nav-notify-icon')
+  .addEventListener('click', function () {
+    active
+      ? myStoreContent.classList.add('my-store-active')
+      : myStoreContent.classList.remove('my-store-active')
 
-// let checked
+    active = !active
+  })
+
+let alertActive = true
+document
+  .querySelector('.nav-notify-btn')
+  .addEventListener('click', function () {
+    alertActive
+      ? alertContent.classList.add('alert-active')
+      : alertContent.classList.remove('alert-active')
+
+    alertActive = !alertActive
+  })
+
+// function
+function updateProgressBar () {
+  const checkedCount = document.querySelectorAll('.checkbox:checked').length
+
+  const totalCheckboxes = checkBoxes.length
+
+  const progressValue = (checkedCount / totalCheckboxes) * 5
+  console.log(progressValue)
+  progress.value = progressValue
+  progEl.textContent = progressValue
+}
+
+// Event Listener
 main.addEventListener('click', function (e) {
   const newClicked = e.target.closest('.checker')
 
   if (!newClicked) return
 
-  contents.forEach(content => content.classList.add('open-content'))
+  checkBoxes.forEach(checkBox => {
+    checkBox.addEventListener('click', function (e) {
+      const clicked = e.target
 
-  // newClicked.classList.add('open-content')
-  // newClicked.nextElementSibling.classList.remove('main-form-body-1-img')
+      if (checkBox.checked) {
+        // change images
+        clicked.previousElementSibling.children[0].classList.add('open-content')
+        clicked.previousElementSibling.children[1].classList.remove(
+          'main-form-body-1-img'
+        )
 
-  newClicked.parentElement.parentElement.nextElementSibling.children[1].children[1].classList.remove(
-    'open-content'
-  )
+        // Close the clicked content
+        clicked.parentElement.nextElementSibling.children[1].classList.add(
+          'open-content'
+        )
+        // Close the clicked content image for destop mode
+        clicked.parentElement.nextElementSibling.nextElementSibling.classList.remove(
+          'main-form-body-1-img-active'
+        )
+        clicked.parentElement.nextElementSibling.nextElementSibling.classList.add(
+          'main-form-body-1-img'
+        )
+        updateProgressBar()
 
-  // for (let i = 0; i < containers.length; i++) {
+        // Check if there is anyone remaining; if true, show its content
+        if (checkBox === checkBoxes[checkBoxes.length - 1]) {
+          let foundUnchecked = false
 
-  //   console.log(containers[i])
-  // }
-  containers.forEach(container => {
-    const iconContainer = container.querySelector('.icons-img')
-    const icon = iconContainer.querySelector('.checked-img')
+          checkBoxes.forEach((checkbox, index) => {
+            if (!checkbox.checked && !foundUnchecked) {
+              // Add the image for desktop mode
 
-    // console.log(
-    //   newClicked.parentElement.parentElement.nextElementSibling.children[0]
-    //     .children[1]
-    // )
+              contents[index].parentElement.nextElementSibling.classList.add(
+                'main-form-body-1-img-active'
+              )
+              contents[index].classList.add('main-form-body-1-desktop')
+              contents[index].classList.remove('open-content') //original code
+              foundUnchecked = true
+            }
+          })
+        }
+        // check if it is the last element; return immedieately if true
+        if (clicked.parentElement.parentElement.nextElementSibling === null)
+          return
 
-    // if (icon.classList.contains('main-form-body-1-img')) {
-    //   console.log('clicked')
-    //   // return
-    // } else {
-    //   console.log('not clicked')
-    // }
+        // Check if the next content is not clicked yet
+        if (
+          !clicked.parentElement.parentElement.nextElementSibling.children[0]
+            .children[1].checked
+        ) {
+          contents.forEach(content => {
+            // Hide all the active classes first
+            content.classList.add('open-content')
+            content.parentElement.nextElementSibling.classList.remove(
+              'main-form-body-1-img-active'
+            )
+          })
+
+          clicked.parentElement.parentElement.nextElementSibling.children[2].classList.add(
+            'main-form-body-1-img-active'
+          )
+
+          clicked.parentElement.parentElement.nextElementSibling.children[1].children[1].classList.remove(
+            'open-content'
+          )
+        }
+      } else {
+        // change the images if unclicked
+        // console.log(clicked.previousElementSibling)
+        clicked.previousElementSibling.children[0].classList.remove(
+          'open-content'
+        )
+        clicked.previousElementSibling.children[1].classList.add(
+          'main-form-body-1-img'
+        )
+        updateProgressBar()
+      }
+    })
   })
 })
-
-// containers.forEach(container => {
-//   container.addEventListener('click', function (e) {
-//     const clickedChecker = e.target
-//     if (!e.target.classList.contains('checker')) return
-
-//     //Change the empty and clicked images
-//     clickedChecker.classList.add('open-content')
-//     clickedChecker.nextElementSibling.classList.remove('main-form-body-1-img')
-
-//     // not to show the content of the current clicked
-//     if (clicked) {
-//       // clickedChecker.parentElement.nextElementSibling.children[1].classList.remove(
-//       //   'open-content'
-//       // )
-//     } else {
-//       clickedChecker.parentElement.nextElementSibling.children[1].classList.add(
-//         'open-content'
-//       )
-//       if (container.contains(clickedChecker))
-//         container.classList.remove('main-form-body-1-opened')
-//     }
-
-//     if (container.nextElementSibling === null) return
-
-//     // container.nextElementSibling.children[1].children[1].classList.remove(
-//     //   'open-content'
-//     // )
-//     // container.nextElementSibling.classList.add('main-form-body-1-opened')
-
-//     // if (
-//     //   !container.nextElementSibling.children[1].children[1].classList.contains(
-//     //     'open-content'
-//     //   )
-//     // ) {
-//     //   // return ''
-//     //   // container.nextElementSibling.children[1].children[1].classList.remove(
-//     //   //   'open-content'
-//     //   // )
-//     //   // container.nextElementSibling.classList.add('main-form-body-1-opened')
-//     // } else {
-//     //   container.nextElementSibling.children[1].children[1].classList.remove(
-//     //     'open-content'
-//     //   )
-//     //   container.nextElementSibling.classList.add('main-form-body-1-opened')
-//     // }
-
-//     //   return
-
-//     // clicked ? (clicked = false) : (clicked = true)
-//     clicked = false
-//   })
-// })
